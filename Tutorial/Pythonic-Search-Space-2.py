38dd45a39b729b8dd1d321814797d9f842bd8012
217kaito <ee217118@meiji.ac.jp>
2024-09-23 16:45:02 +0900

    Pythonic-Search-Space-2.pyの作成

---
+++ Tutorial/Pythonic-Search-Space-2.py
@@ -0,0 +1,12 @@
+import sklearn.ensemble
+import sklearn.svm
+
+# パラメータ値に応じて分岐を使用することができる
+def objective(trial):
+    classifier_name = trial.suggest_categorical("classifier", ["SVC", "RandomForest"])
+    if classifier_name == "SVC":
+        svc_c = trial.suggest_float("svc_c", 1e-10, 1e10, log=True)
+        classifier_obj = sklearn.svm.SVC(C=svc_c)
+    else:
+        rf_max_depth = trial.suggest_int("rf_max_depth", 2, 32, log=True)
+        classifier_obj = sklearn.ensemble.RandomForestClassifier(max_depth=rf_max_depth)
